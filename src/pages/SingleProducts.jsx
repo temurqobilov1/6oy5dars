a7334a0ff6bf3139c7b4dc9eaf381d5b621af3f2
temurqobilov1 <qobilovtemuruz@gmail.com>
2025-08-05 23:31:02 +0500

    done

--- src/pages/SingleProducts.jsx
+++ src/pages/SingleProducts.jsx
@@ -1,91 +1,62 @@
-import { NavLink } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+import { useParams } from "react-router-dom";
 
 function SingleProducts() {
+  const { id } = useParams();
+  const {
+    data: product,
+    isPending,
+    error,
+  } = useFetch("https://dummyjson.com/product/" + id);
+
+  if (isPending) {
+    return (
+      <div className="flex items-center justify-center mt-60">
+        <span className="loading loading-spinner loading-xl "></span>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center mt-60">
+        <h1 className="text-2xl italic">{error}</h1>
+      </div>
+    );
+  }
   return (
-    <form
-      action=""
-      className="w-[460px]  mt-10 mx-auto bg-[#f2f4f7]  flex flex-col gap-4 py-3 px-6  rounded-xl shadow-2xl"
-    >
-      <div className="mx-auto mb-4">
-        <h1 className="text-5xl font-bold text-[#166fe5]">Sign up</h1>
-      </div>
-      <div className="mx-auto mb-3">
-        <input
-          className="border text-black px-3 rounded text-xl w-[360px] h-10"
-          type="text"
-          placeholder="Name"
-        />
-      </div>
-      <div className="mx-auto mb-3">
-        <input
-          className="border text-black px-3 rounded text-xl w-[360px] h-10"
-          type="text"
-          placeholder="Surname"
-        />
-      </div>
-      <div className="mx-auto mb-3">
-        <input
-          className="border text-black px-3 rounded text-xl w-[360px] h-10 hover:cursor-pointer"
-          type="date"
-        />
-      </div>
-      <div className="mx-auto mb-3 flex items-center gap-5">
-        <label className="text-xl  hover:cursor-pointer" id="male">
-          <div className="flex items-center justify-around border w-[170px] px-3 rounded text-2xl h-10  hover:cursor-pointer">
-            Male
-            <input
-              className=" hover:cursor-pointer "
-              type="radio"
-              id="male"
-              name="typeone"
-            />
+    <>
+      <h2 className="text-center text-3xl my-7">ProducList</h2>
+      {product && (
+        <div className="flex items-center justify-between p-10 shadow-xl/30 max-w-4xl mx-auto rounded-2xl">
+          <div className="w-[40%]">
+            <img className="" src={product.thumbnail} alt="Album" />
           </div>
-        </label>
-        <label className="text-xl  hover:cursor-pointer" id="female">
-          <div className="flex items-center justify-around border w-[170px] px-3 rounded text-2xl h-10  hover:cursor-pointer">
-            Female
-            <input
-              className="hover:cursor-pointer "
-              type="radio"
-              id="female"
-              name="typeone"
-            />
+          <div className=" w-[60%]">
+            <h2 className="text-4xl mb-3">{product.title}</h2>
+            <p className="text-3xl font-bold mb-3 text-[#eb5971]">
+              $<span className="text-3xl">{product.price}</span>
+            </p>
+            <p className="mb-3">{product.description}</p>
+            <p className="text-xl font-bold">
+              Brand:{" "}
+              <span className="font-normal italic text-[#00a9e7]">
+                {product.brand}
+              </span>
+            </p>
+            <div className="flex mt-4 justify-between">
+              <p className="text-xl font-bold">
+                Rating:{" "}
+                <span className="font-bold text-2xl text-[#eb5971]">
+                  {product.rating}
+                </span>
+              </p>
+            </div>
           </div>
-        </label>
-      </div>
-      <div className="mx-auto mb-3">
-        <input
-          className="border text-black px-3 rounded text-xl w-[360px] h-10"
-          type="email"
-          placeholder="Phone number or email address"
-        />
-      </div>
-      <div className="mx-auto mb-3">
-        <input
-          className="border text-black px-3 rounded text-xl w-[360px] h-10"
-          type="password"
-          placeholder="New password"
-        />
-      </div>
-      <div className="mx-auto mb-3">
-        <button
-          type="submit"
-          className="border text-white rounded text-2xl w-[360px] h-13 bg-[#166fe5] hover:cursor-pointer"
-        >
-          Sign up
-        </button>
-      </div>
-      <div className="mx-auto mb-3">
-        <NavLink to="/login">
-          <p className="italic text-[#166fe5] hover:underline">
-            Do you already have an account?
-          </p>
-        </NavLink>
-      </div>
-    </form>
+        </div>
+      )}
+    </>
   );
 }
 
 export default SingleProducts;
-
-
