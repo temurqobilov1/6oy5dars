38d933f74e92dbf76f95b10c9cb24094fe10a910
temurqobilov1 <qobilovtemuruz@gmail.com>
2025-08-07 18:24:09 +0500

    done

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,11 +1,7 @@
-import { useState } from "react";
-
 export function useLogin() {
-  const [isPanding, setIsPanding] = useState(false);
-
   const login = (email, password) => {
     console.log(email, password);
   };
 
-  return { login, isPanding };
+  return { login };
 }
--- src/hooks/useRegister.js
+++ src/hooks/useRegister.js
@@ -1,10 +1,7 @@
-import { useState } from "react";
-
 export function useRegister() {
-  const [isPanding, setIsPanding] = useState(false);
   const register = async (firstName, lastName, email, password) => {
     console.log(firstName, lastName, email, password);
   };
 
-  return { register, isPanding };
+  return { register };
 }
